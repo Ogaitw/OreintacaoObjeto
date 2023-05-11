ef0a51ee08471c372db70a668c69e1b4dc996ffc
Ogaitw <tiagoaugusto2402@gmail.com>
2023-05-11 13:57:39 -0300

    Commit -2

---
+++ src/associacao/teste/jogadorTeste02.java
@@ -0,0 +1,15 @@
+package associacao.teste;
+
+import associacao.dominio.Jogador;
+import associacao.dominio.Time;
+
+public class jogadorTeste02 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Time time = new Time("Seleção Brasileira");
+
+        jogador1.setTime(time);
+        jogador1.imprime();
+
+    }
+}
