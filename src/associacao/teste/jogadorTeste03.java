1693c1dcd959c7981dcf08a369e3975c652875f3
Ogaitw <tiagoaugusto2402@gmail.com>
2023-05-15 18:29:29 -0300

    Ate associação

---
+++ src/associacao/teste/jogadorTeste03.java
@@ -0,0 +1,22 @@
+package associacao.teste;
+
+import associacao.dominio.Jogador;
+import associacao.dominio.Time;
+
+public class jogadorTeste03 {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Cafu");
+        Jogador jogador2 = new Jogador("Pele");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador, jogador2};
+        jogador2.setTime(time);
+        jogador.setTime(time);
+        time.setJogadores(jogadores);
+
+        System.out.println("===== Jogador====");
+        jogador.imprime();
+        System.out.println("===== Time====");
+        time.imprime();
+
+    }
+}
